fae946569538d66a5e68041cc300f9e8c174af72
Huynh Tan Loc <12a1huynhtanloc@gmail.com>
2025-02-02 22:53:25 +0700

    Basic GraphQL

--- index.ts
+++ index.ts
@@ -1,20 +1,37 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import * as database from "./config/database";
 import dotenv from "dotenv";
-import Article from "./models/article.model";
+import { ApolloServer, gql } from "apollo-server-express";
+
+import { typeDefs } from "./typeDefs";
+import { resolvers } from "./resolvers";
 
 const app: Express = express();
 
 dotenv.config();
 
 const port: number | string = process.env.PORT || 3000;
-database.connect();
 
-app.get("/articles", async (req: Request, res: Response): Promise<void> => {
-  const articles = await Article.find({ deleted: false });
-  res.status(200).json(articles);
-});
+const startSever = async () => {
+  database.connect();
 
-app.listen(port, (): void => {
-  return console.log(`Server is listening on ${port}`);
-});
+  // GraphQL
+
+  const apolloSever = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await apolloSever.start();
+
+  apolloSever.applyMiddleware({
+    app: app,
+    path: "/graphql",
+  });
+
+  app.listen(port, (): void => {
+    return console.log(`Server is listening on ${port}`);
+  });
+};
+
+startSever();
